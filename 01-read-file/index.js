e2919ed57ef84793da8a0c9d3d029721dfb493c7
frontend200tb <frontend200tb@mail.ru>
2024-01-18 14:35:27 +0300

    feat: do task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+const input = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+input.on('data', data => process.stdout.write(data));
